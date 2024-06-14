2db3303bc6663f211eb34fa6e4eea9a23fc7c13f
sy0223lee <sy0223lee@gmail.com>
2024-06-15 00:01:18 +0900

    feat: 네이버 로그인 접근 토큰 받기
    응답이 안 오는 오류 발생...

--- src/main/java/com/multi/hereevent/member/NaverLoginController.java
+++ src/main/java/com/multi/hereevent/member/NaverLoginController.java
@@ -1,11 +1,10 @@
 package com.multi.hereevent.member;
 
 import org.json.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.math.BigInteger;
@@ -21,32 +20,36 @@ public class NaverLoginController {
 
         // 네이버 로그인 인증 요청문 생성
         String url = "https://nid.naver.com/oauth2.0/authorize?client_id=" + "08J45Bf5olDzizmNsIcJ" +
-                    "&response_type=code&redirect_uri=" + "http://localhost:9090/hereevnet/login/naver/callback" +
+                    "&response_type=code&redirect_uri=http://localhost:9090/hereevent/login/naver/callback" +
                     "&state=" + state;
 
         System.out.println("===== 네이버 로그인 인증 요청 =====");
         return "redirect:" + url;
     }
 
-    @RequestMapping(value = "/login/naver/callback",
-            method = {RequestMethod.GET, RequestMethod.POST},
-            produces = "application/json")
+    @GetMapping(value = "/login/naver/callback", produces = "application/json")
+    @ResponseBody
     public void naverLogin(@RequestParam("code") String code, @RequestParam("state") String state){
-        // 여기로 안 넘어와....
         System.out.println("===== 네이버 로그인 접근 토큰 요청 =====");
-        // 접근 토큰 요청문 생성
-        String url = "https://nid.naver.com/oauth2.0/token" +
-                "?client_id=" + "08J45Bf5olDzizmNsIcJ" +
-                "&client_secret=" + "53JS4ixZ2M" +
-                "&grant_type=authorization_code" +
-                "&state=" + "state" +
-                "&code=" + "code";
-
         // POST 요청 보내고 응답 JSON 객체로 저장
-        WebClient client = WebClient.create(url);
+        WebClient client = WebClient.builder()
+                .baseUrl("https://nid.naver.com")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
         JSONObject response = client.post()
-                                    .retrieve().bodyToMono(JSONObject.class).block();
+                .uri(uriBuilder -> uriBuilder
+                        .path("/oauth2.0/token")
+                        .queryParam("grant_type", "authorization_code")
+                        .queryParam("client_id", "08J45Bf5olDzizmNsIcJ")
+                        .queryParam("client_secret", "53JS4ixZ2M")
+                        .queryParam("code", code)
+                        .queryParam("state", state)
+                        .build())
+                .retrieve().bodyToMono(JSONObject.class).block();
+
         System.out.println("===== 네이버 로그인 접근 토큰 응답 =====");
+        // response가 아무것도 안넘어와...!
+        System.out.println("[토큰] "+ response);
         // JSON 에서 접근 토큰 추출
         String token = response.getString("access_token");
         System.out.println("===== 네이버 로그인 사용자 정보 요청 =====");
