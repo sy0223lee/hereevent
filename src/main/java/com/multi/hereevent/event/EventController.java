de3d5218cae2d3b61cdf5774ead1c9900e13faa8
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-06-19 17:34:35 +0900

    Update EventController.java

--- src/main/java/com/multi/hereevent/event/EventController.java
+++ src/main/java/com/multi/hereevent/event/EventController.java
@@ -1,17 +1,15 @@
 package com.multi.hereevent.event;
 
 import com.multi.hereevent.dto.EventDTO;
+import com.multi.hereevent.dto.ReservationDTO;
 import com.multi.hereevent.dto.ReviewDTO;
+import com.multi.hereevent.fileupload.FileUploadService;
 import com.multi.hereevent.review.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -21,13 +19,24 @@ public class EventController {
     private final EventService eventService;
     private final ReviewService reviewService;
 
-    @GetMapping("/main")
-    public String mainPage() {
-        return "main/test";
-    }
     @GetMapping("/test")
     public String test() {
-        return "main/Test";
+        return "main/bootTest";
+    }
+
+    @GetMapping("/main")
+    public String mainPage() {
+        return "main/mainPage";
+    }
+    @GetMapping("/test2")
+    public String test2(Model model) {
+        List<EventDTO> alleventlist = service.getAllEvent();
+        model.addAttribute("alleventlist",alleventlist);
+        List<EventDTO> openlist = service.getOpenEvent();
+        model.addAttribute("openlist",openlist);
+        List<EventDTO> popularlist = service.getPopularEvent();
+        model.addAttribute("popularlist",popularlist);
+        return "main/mainPage2";
     }
 
 
@@ -40,82 +49,64 @@ public class EventController {
         return mav;
     }
 
-    //전체행사조회(프론트 아직)
-    @GetMapping("/alleventlist")
-    public ModelAndView getAllEvent(){
-        ModelAndView mav = new ModelAndView("main/alllistTest");
-        List<EventDTO> alleventlist = eventService.getAllEvent();
-        mav.addObject("alleventlist",alleventlist);
-        return mav;
-    }
-
-    //오픈예정행사(프론트 아직)
-    @GetMapping("/openlist")
-    public ModelAndView getOpenEvent(){
-        ModelAndView mav = new ModelAndView("main/listTest");
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String today = sdf.format(new Date());
-        List<EventDTO> openlist = eventService.getOpenEvent(today);
-        mav.addObject("openlist",openlist);
-        mav.addObject("today", today);
-        return mav;
-    }
-
-    //예약/대기많은행사 top10(프론트 아직)
-    @GetMapping("/popularlist")
-    public ModelAndView getPopularEvent(){
-        ModelAndView mav = new ModelAndView("main/popularlistTest");
-        List<EventDTO> popularlist = eventService.getPopularEvent();
-        mav.addObject("popularlist",popularlist);
-        return mav;
-    }
-
 //   세부페이지
     @GetMapping("/{event_no}")
     public String getEventDetails(@PathVariable("event_no") int event_no, Model model) {
-        EventDTO eventDetails = eventService.getEventDetails(event_no);
+        EventDTO eventDetails = service.getEventDetails(event_no);
         model.addAttribute("event", eventDetails);
         return "detailedPage/detailedPage";
     }
-    //상세정보
-    @GetMapping("/content/{event_no}")
-    public String showContent(@PathVariable("event_no") int event_no, Model model) {
-        EventDTO eventDetails = eventService.getEventDetails(event_no);
-        model.addAttribute("event", eventDetails);
-        return "detailedPage/content";
-    }
-    //길찾기
-    @GetMapping("/navigation/{event_no}")
-    public String showNavigation(@PathVariable("event_no") int event_no, Model model) {
-        EventDTO eventDetails = eventService.getEventDetails(event_no);
-        model.addAttribute("event", eventDetails);
-        return "detailedPage/navigation";
-    }
-    //예약
-    @GetMapping("/reservation/{event_no}")
-    public String showReservation(@PathVariable("event_no") int event_no, Model model) {
-        EventDTO eventDetails = eventService.getEventDetails(event_no);
-        model.addAttribute("event", eventDetails);
 
-        return "detailedPage/reservation";
-    }
-    //후기
-    @GetMapping("/review/{event_no}")
-    public String showReview(@PathVariable("event_no") int event_no, Model model) {
-        EventDTO eventDetails = eventService.getEventDetails(event_no);
-        List<ReviewDTO> reviewList = reviewService.selectReviewByEventNo(event_no);
-        model.addAttribute("event", eventDetails);
-        model.addAttribute("reviewList", reviewList);
-        return "detailedPage/review";
-    }
+//    //상세정보 (*스크롤형식으로 바꿔 필요 없어져 주석처리*)
+//    @GetMapping("/content/{event_no}")
+//    public String showContent(@PathVariable("event_no") int event_no, Model model) {
+//        EventDTO eventDetails = service.getEventDetails(event_no);
+//        model.addAttribute("event", eventDetails);
+//        return "detailedPage/content";
+//    }
+//    //길찾기
+//    @GetMapping("/navigation/{event_no}")
+//    public String showNavigation(@PathVariable("event_no") int event_no, Model model) {
+//        EventDTO eventDetails = service.getEventDetails(event_no);
+//        model.addAttribute("event", eventDetails);
+//        return "detailedPage/navigation";
+//    }
+//    //예약
+//    @GetMapping("/reservation/{event_no}")
+//    public String showReservation(@PathVariable("event_no") int event_no, Model model) {
+//        System.out.println("reservatation");
+//        EventDTO eventDetails = service.getEventDetails(event_no);
+//        model.addAttribute("event", eventDetails);
+//
+//        return "detailedPage/reservation";
+//    }
+//    //후기
+//    @GetMapping("/review/{event_no}")
+//    public String showReview(@PathVariable("event_no") int event_no, Model model) {
+//        System.out.println("review");
+//        EventDTO eventDetails = service.getEventDetails(event_no);
+//        model.addAttribute("event", eventDetails);
+//        return "detailedPage/review";
+//    }
+
 
     //이벤트 사진 가져오기
-    @GetMapping("/image/{event_no}")
+    @GetMapping("/image/{eventNo}")
     @ResponseBody
     public EventDTO getEventImage(@PathVariable("event_no") int event_no, Model model) {
-        EventDTO eventDetails = eventService.getEventDetails(event_no);
+        EventDTO eventDetails = service.getEventDetails(event_no);
         model.addAttribute("event", eventDetails);
-        return eventService.getEventImage(event_no);
+        return service.getEventImage(event_no);
+    }
+
+    //카테고리별 리스트
+    @GetMapping("/list")
+    public String listCategory(@RequestParam("category_no") int category_no, Model model){
+        //System.out.println("NO==>>"+ category_no);
+        List<EventDTO> eventlist = service.selectEventByCategoryNo(category_no);
+       // System.out.println("eventlist=>>>>"+eventlist);
+        model.addAttribute("eventlist",eventlist);
+        return "event/eventCategoryList";
     }
 }
 
