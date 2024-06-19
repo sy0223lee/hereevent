e962b687fa0b63159fab8ccc4080bc35f6eda1fd
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-06-19 13:17:43 +0900

    Update HereEventConfig.java

--- src/main/java/com/multi/hereevent/config/HereEventConfig.java
+++ src/main/java/com/multi/hereevent/config/HereEventConfig.java
@@ -11,6 +11,6 @@ public class HereEventConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 특정 path 로 요청하면 실제 파일이 저장된 위치를 연결해서 리소스를 가져올 수 있도록 처리
         registry.addResourceHandler("/download/**") // '/download' 로 시작하는 모든 요청에 대해
-                .addResourceLocations("file:///C:/hereevent_upload/event/"); // 실제 위치에서 파일 찾기
+                .addResourceLocations("file:///C:/hereevent_upload/"); // 실제 위치에서 파일 찾기
     }
 }
