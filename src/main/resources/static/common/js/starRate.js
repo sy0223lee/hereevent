dd42f27eed822c89643db9f31e1af918d572b5b3
sy0223lee <sy0223lee@gmail.com>
2024-06-21 01:43:40 +0900

    feat: 리뷰 별점 등록, 출력
    star 이미지 추가
    starRate.css, starRate.js 추가
    불필요한 이미지 제거

--- src/main/resources/static/common/js/starRate.js
+++ src/main/resources/static/common/js/starRate.js
@@ -1,78 +1,70 @@
-const rateWrap = document.querySelectorAll('.rating'),
-    label = document.querySelectorAll('.rating .rating-label'),
-    input = document.querySelectorAll('.rating .rating-input'),
-    labelLength = label.length,
-    opacityHover = '0.5';
+$(document).ready(function() {
+    const rateWrap = document.querySelectorAll('.rating');
+    const label = document.querySelectorAll('.rating .rating-label');
+    const input = document.querySelectorAll('.rating .rating-input');
+    const labelLength = label.length;
+    let stars = document.querySelectorAll('.rating .star-icon');
 
-let stars = document.querySelectorAll('.rating .star-icon');
+    checkedRate();
 
-checkedRate();
+    rateWrap.forEach(wrap => {
+        wrap.addEventListener('mouseover', () => {
+            stars = wrap.querySelectorAll('.star-icon');
 
-rateWrap.forEach(wrap => {
-    wrap.addEventListener('mouseenter', () => {
-        stars = wrap.querySelectorAll('.star-icon');
-
-        stars.forEach((starIcon, idx) => {
-            starIcon.addEventListener('mouseenter', () => {
-                initStars();
-                filledRate(idx, labelLength);
-
-                for (let i = 0; i < stars.length; i++) {
-                    if (stars[i].classList.contains('filled')) {
-                        stars[i].style.opacity = opacityHover;
+            stars.forEach((starIcon, idx) => {
+                starIcon.addEventListener('mouseenter', () => {
+                    if (wrap.classList.contains('readonly') === false) {
+                        initStars(); // 별점 초기화
+                        filledRate(idx, labelLength); // 마우스 호버된 만큼 별점 채우기
                     }
-                }
-            });
+                });
 
-            starIcon.addEventListener('mouseleave', () => {
-                starIcon.style.opacity = '1';
-                checkedRate();
-            });
-
-            wrap.addEventListener('mouseleave', () => {
-                starIcon.style.opacity = '1';
+                starIcon.addEventListener('mouseleave', () => {
+                    checkedRate(); // 체크된 라디오 버튼만큼 별점 active
+                });
             });
         });
     });
-});
 
-function filledRate(index, length) {
-    if (index <= length) {
-        for (let i = 0; i <= index; i++) {
-            stars[i].classList.add('filled');
-        }
-    }
-}
-
-function checkedRate() {
-    let checkedRadio = document.querySelectorAll('.rating input[type="radio"]:checked');
-
-
-    initStars();
-    checkedRadio.forEach(radio => {
-        let previousSiblings = prevAll(radio);
-
-        for (let i = 0; i < previousSiblings.length; i++) {
-            previousSiblings[i].querySelector('.star-icon').classList.add('filled');
-        }
-
-        radio.nextElementSibling.classList.add('filled');
-
-        function prevAll() {
-            let radioSiblings = [],
-                prevSibling = radio.parentElement.previousElementSibling;
-
-            while (prevSibling) {
-                radioSiblings.push(prevSibling);
-                prevSibling = prevSibling.previousElementSibling;
+    // 가리키고 있는 별점보다 낮은 별들은 채워주기
+    function filledRate(index, length) {
+        if (index <= length) {
+            for (let i = 0; i <= index; i++) {
+                stars[i].classList.add('filled');
             }
-            return radioSiblings;
         }
-    });
-}
-
-function initStars() {
-    for (let i = 0; i < stars.length; i++) {
-        stars[i].classList.remove('filled');
     }
-}
+
+    // 선택된 별점보다 낮은 별들은 채워주기
+    function checkedRate() {
+        let checkedRadio = document.querySelectorAll('.rating input[type="radio"]:checked');
+
+        initStars();
+        checkedRadio.forEach(radio => {
+            let previousSiblings = prevAll(radio);
+
+            for (let i = 0; i < previousSiblings.length; i++) {
+                previousSiblings[i].querySelector('.star-icon').classList.add('filled');
+            }
+
+            radio.nextElementSibling.classList.add('filled');
+
+            function prevAll() {
+                let radioSiblings = [];
+                let prevSibling = radio.parentElement.previousElementSibling;
+
+                while (prevSibling) {
+                    radioSiblings.push(prevSibling);
+                    prevSibling = prevSibling.previousElementSibling;
+                }
+                return radioSiblings;
+            }
+        });
+    }
+
+    function initStars() {
+        for (let i = 0; i < stars.length; i++) {
+            stars[i].classList.remove('filled');
+        }
+    }
+});
