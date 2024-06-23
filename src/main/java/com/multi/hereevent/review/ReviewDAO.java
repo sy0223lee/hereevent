05b3454c0ac32ca3ade7a0f47ead7c3c70af3652
sy0223lee <sy0223lee@gmail.com>
2024-06-24 00:28:51 +0900

    feat: 리뷰 이미지 출력

--- src/main/java/com/multi/hereevent/review/ReviewDAO.java
+++ src/main/java/com/multi/hereevent/review/ReviewDAO.java
@@ -11,5 +11,6 @@ public interface ReviewDAO {
     int updateReview(ReviewDTO review); // 리뷰 수정
     int deleteReview(int review_no); // 리뷰 삭제
     List<ReviewDTO> selectReviewByEventNo(int event_no); // 이벤트 번호에 해당하는 리뷰 조회
-    List<ReviewDTO> selectReviewByMemberNo(int member_no); // 회원 번호에 해당하는 리뷰 조회
+    List<ReviewDTO> selectReviewByMemberNo(int member_no);
+    List<ReviewImgDTO> selectReviewImgs(int review_no);// 회원 번호에 해당하는 리뷰 조회
 }
