be9586b4e30b4a97ab2acd139f1e743f170a3641
jhyuk23 <jkt6869@naver.com>
2024-06-25 17:29:49 +0900

    관심 카테고리 수정

--- src/main/java/com/multi/hereevent/dto/CategoryInterestDTO.java
+++ src/main/java/com/multi/hereevent/dto/CategoryInterestDTO.java
@@ -12,19 +12,15 @@ import org.apache.ibatis.type.Alias;
 public class CategoryInterestDTO {
     private int category_interest_no;
     private int member_no;
-    private int category1;
-    private int category2;
-    private int category3;
+    private int category_no;
 
     // 회원이 관심있는 카테고리의 이름을 저장할 멤버 변수
     private String name;
 
-    public CategoryInterestDTO(int category_interest_no, int member_no, int category1, int category2, int category3) {
+    public CategoryInterestDTO(int category_interest_no, int member_no, int category_no) {
         this.category_interest_no = category_interest_no;
         this.member_no = member_no;
-        this.category1 = category1;
-        this.category2 = category2;
-        this.category3 = category3;
+        this.category_no = category_no;
     }
 }
 
