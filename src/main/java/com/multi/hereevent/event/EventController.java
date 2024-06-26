26e719a609365121efe8b31642c5caed7dcd5e89
sy0223lee <sy0223lee@gmail.com>
2024-06-26 09:27:20 +0900

    fix: 빠진 import 추가, 경로 수정

--- src/main/java/com/multi/hereevent/event/EventController.java
+++ src/main/java/com/multi/hereevent/event/EventController.java
@@ -2,6 +2,7 @@ package com.multi.hereevent.event;
 
 import com.multi.hereevent.dto.EventDTO;
 import com.multi.hereevent.dto.MemberDTO;
+import com.multi.hereevent.dto.ReserveDTO;
 import com.multi.hereevent.event.interest.EventInterestService;
 import com.multi.hereevent.dto.ReviewDTO;
 import com.multi.hereevent.review.ReviewService;
