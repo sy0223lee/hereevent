8d793845b7089b5199953ebb449545e4337d8be6
jhyuk23 <jkt6869@naver.com>
2024-06-27 14:56:12 +0900

    -reservation time 수정

--- src/main/java/com/multi/hereevent/event/time/EventTimeService.java
+++ src/main/java/com/multi/hereevent/event/time/EventTimeService.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface EventTimeService {
     int insertEventTimeList(List<EventTimeDTO> eventTimeList);
+    List<String> getOperTime(int event_no, String day);
+    EventTimeDTO getEventTimeByEventNoAndDay(int event_no, String day);
 }
