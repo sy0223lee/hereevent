5698c4e8979abe53d63487a9d380f034b92657dc
sy0223lee <sy0223lee@gmail.com>
2024-07-02 13:01:40 +0900

    minor: 크롤링 redirect 경로 수정

--- src/main/java/com/multi/hereevent/event/crawling/CrawlingService.java
+++ src/main/java/com/multi/hereevent/event/crawling/CrawlingService.java
@@ -49,7 +49,8 @@ public class CrawlingService {
         cal.add(Calendar.DATE, 14); // 2주
         String day = sdf.format(cal.getTime());
         try {
-            driver.get("https://popply.co.kr/popup?area=all&startDate=" + day + "&endDate=" + today + "&status=all");
+            driver.get("https://popply.co.kr/popup?area=all&startDate=" + today + "&endDate=" + "2024-06-15" + "&status=all");
+//            driver.get("https://popply.co.kr/popup?area=all&startDate=" + day + "&endDate=" + today + "&status=all");
             WebElement element = driver.findElement(By.cssSelector("div.calendar-popup-list.popuplist-board"));
 
             // 팝업 이미지, 상세 정보 리스트에 저장
